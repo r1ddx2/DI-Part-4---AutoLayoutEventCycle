0793c37e05302a25ee9bb21d2eade765b65999a6
Red Wang <redxoxored@gmail.com>
2023-10-25 23:12:24 +0800

    feat: Add color extension

--- DI-Part 4-AutoLayoutEventCycle/UIColor+Extension.swift
+++ DI-Part 4-AutoLayoutEventCycle/UIColor+Extension.swift
@@ -5,4 +5,20 @@
 //  Created by Red Wang on 2023/10/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, alpha: Float = 1.0) {
+        
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(alpha)
+        )
+    }
+    
+    static let yellowCircleColor = UIColor(red: 255, green: 204, blue: 1)
+    static let purpleCircleColor = UIColor(red: 88, green: 86, blue: 214)
+    static let redCircleColor = UIColor(red: 255, green: 44, blue: 85)
+}
