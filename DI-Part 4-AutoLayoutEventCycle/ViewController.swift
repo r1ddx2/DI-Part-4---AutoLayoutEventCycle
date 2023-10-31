89e403e230238f69fdf5c588ce474f28dedd07d5
Red Wang <redxoxored@gmail.com>
2023-10-31 13:02:32 +0800

    fix: Use `viewDidLayoutSubview` to fix orientation bugs

--- DI-Part 4-AutoLayoutEventCycle/ViewController.swift
+++ DI-Part 4-AutoLayoutEventCycle/ViewController.swift
@@ -12,59 +12,59 @@ class ViewController: UIViewController {
     let yellowCircle = UIView()
     let purpleCircle = UIView()
     let redCircle = UIView()
-    
-    let bigCircleRadius = UIScreen.main.bounds.width / 2.0
-    let smallCircleRadius = UIScreen.main.bounds.width / 4.0
-    
+   
+    override func viewDidLayoutSubviews() {
+        setUpCircles()
+        setUpLayouts()
+        view.layoutIfNeeded()
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setUpCircles()
-        setUpLayouts()
-        setUpBigCircleLayouts()
+      
     }
     
     private func setUpCircles(){
-        yellowCircle.layer.cornerRadius = bigCircleRadius * 0.5
+        let yellowRadius = view.frame.width / 4.0
+        yellowCircle.layer.cornerRadius = yellowRadius
         yellowCircle.backgroundColor = UIColor.yellowCircleColor
-        
-        purpleCircle.layer.cornerRadius = smallCircleRadius * 0.5
+    
+        purpleCircle.layer.cornerRadius = yellowRadius / 2.0
         purpleCircle.backgroundColor = UIColor.purpleCircleColor
         
-        redCircle.layer.cornerRadius = smallCircleRadius * 0.5
+        redCircle.layer.cornerRadius = yellowRadius / 2.0
         redCircle.backgroundColor = UIColor.redCircleColor
     }
+    
     private func setUpLayouts() {
         view.addSubview(yellowCircle)
-        
         yellowCircle.translatesAutoresizingMaskIntoConstraints = false
-        
+   
         NSLayoutConstraint.activate([
             yellowCircle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             yellowCircle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            yellowCircle.widthAnchor.constraint(equalToConstant: bigCircleRadius),
-            yellowCircle.heightAnchor.constraint(equalToConstant: bigCircleRadius)
+            yellowCircle.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            yellowCircle.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
         ])
-    }
-    private func setUpBigCircleLayouts(){
+  
+        // MARK: - Yellow Circle View
         yellowCircle.addSubview(purpleCircle)
         yellowCircle.addSubview(redCircle)
-        
         yellowCircle.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         
         NSLayoutConstraint.activate([
             purpleCircle.centerYAnchor.constraint(equalTo: yellowCircle.centerYAnchor),
-            purpleCircle.widthAnchor.constraint(equalToConstant: smallCircleRadius),
-            purpleCircle.heightAnchor.constraint(equalToConstant: smallCircleRadius),
             purpleCircle.leadingAnchor.constraint(equalTo: yellowCircle.leadingAnchor),
+            purpleCircle.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25),
+            purpleCircle.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25),
             
             redCircle.leadingAnchor.constraint(equalTo: purpleCircle.trailingAnchor),
-            redCircle.trailingAnchor.constraint(equalTo: yellowCircle.trailingAnchor),
             redCircle.centerYAnchor.constraint(equalTo: yellowCircle.centerYAnchor),
-            redCircle.widthAnchor.constraint(equalToConstant: smallCircleRadius),
-            redCircle.heightAnchor.constraint(equalToConstant: smallCircleRadius)
+            redCircle.trailingAnchor.constraint(equalTo: yellowCircle.trailingAnchor),
+            redCircle.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25),
+            redCircle.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25)
         ])
-        
     }
    
 }
